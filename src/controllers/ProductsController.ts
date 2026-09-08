d81316ba824eeec44abcf61bb14389d910cdb94b
Pedro <pedrohenriquevm215@gmail.com>
2026-09-08 00:10:10 -0300

    feat: add pasta utils para tratamento de error e add Zod para validacoes

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import { AppError } from "../utils/AppError";
+import { z } from "zod"
 
 export class ProductController {
 
@@ -9,9 +11,32 @@ export class ProductController {
     }
 
     create(req: Request, res: Response) {
-        const { name, price } = req.body
+        const bodySchema = z.object({
+            name: z
+            .string({ required_error: "Name is required" })
+            .trim()
+            .min(4, { message: "Name must be 6 or more characters" }),
 
-        throw new Error("ERRO DE EXEMPLO")
+            price: z
+            .number({ required_error: "Price is required" })
+            .positive({ message: "Price must be positive" }),
+        })
+
+        const { name, price } = bodySchema.parse(req.body)
+
+        /*
+        if (!name) {
+            throw new AppError("Nome do produto é obrigatório!")
+
+        }
+        if (!price) {
+            throw new AppError("Preco do produto é obrigatório!")
+
+        }
+        */
+
+        // throw new Error("Erro ao tentar criar um produto")
+        // throw new AppError("Erro ao tentar criar um produto")
 
         res.status(201).json({ name, price, user_id: req.user_id })
 
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response, NextFunction } from "express"
 import { routes } from "./routes"
+import { AppError } from "./utils/AppError"
+import { ZodError } from "zod"
 
 const PORT = 3333
 
@@ -12,6 +14,15 @@ app.use(express.json())
 app.use(routes)
 
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+        return res.status(error.statusCode).json({ message: error.message })
+
+    }
+
+    if (error instanceof ZodError) {
+         return res.status(400).json({ message: "Validation error!", issues: error.format() })
+    }
+
     res.status(500).json({ message: "Erro no servidor!" })
 })
 
